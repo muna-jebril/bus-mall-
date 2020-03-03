b5427bbb1288357688f66276a5ecc47ed6397b78
muna <muna.jebril1995@hotmail.com>
2020-03-03 20:18:25 +0200

    not finished

--- javascript.js
+++ javascript.js
@@ -1,30 +1,34 @@
 var names = [
-    "bag.jpg",
-    "banana.jpg",
-    "bathroom.jpg",
-    "boots.jpg",
-    "breakfast.jpg",
-    "bubblegum.jpg",
-    "chair.jpg",
-    "cthulhu.jpg",
-    "dog-duck.jpg",
-    "dragon.jpg",
-    "pen.jpg",
-    "pet-sweep.jpg",
-    "scissors.jpg",
-    "shark.jpg",
-    "sweep.png",
-    "tauntaun.jpg",
-    "unicorn.jpg",
-    "usb.gif",
-    "water-can.jpg",
-    "wine-glass.jpg",
+  "bag.jpg",
+  "banana.jpg",
+  "bathroom.jpg",
+  "boots.jpg",
+  "breakfast.jpg",
+  "bubblegum.jpg",
+  "chair.jpg",
+  "cthulhu.jpg",
+  "dog-duck.jpg",
+  "dragon.jpg",
+  "pen.jpg",
+  "pet-sweep.jpg",
+  "scissors.jpg",
+  "shark.jpg",
+  "sweep.png",
+  "tauntaun.jpg",
+  "unicorn.jpg",
+  "usb.gif",
+  "water-can.jpg",
+  "wine-glass.jpg",
 ];
 var leftImage = document.querySelector('#leftImage');
 var rightImage = document.querySelector('#rightImage');
-var centerImage=document.querySelector('#centerImage');
+var centerImage = document.querySelector('#centerImage');
 
 var imageSection = document.querySelector('#imagesSection')
+var product =[];
+var tClick =[];
+var total=[];
+var totalView= 0;
 
 
 function Think(name) {
@@ -33,76 +37,165 @@ function Think(name) {
   this.views = 0;
   this.imagePath = `imgs/${this.name}`;
   Think.all.push(this);
-}
-Think.all =[];
+  product.push(this.name);
+  console.log(product);
+ 
+  // tClick.push(this.clicks);
+  // console.log(tClick);
 
-for(var i =0; i<names.length; i++) {
+  
+}
+Think.all = [];
+function saveValues (){
+  localStorage.setItem("Click",totalClicks);
+  localStorage.setItem("views",totalView);
+  
+
+}
+function   getValues(){
+  localStorage.getItem("clicks");
+  localStorage.getItem("views");
+
+
+}
+
+
+for (var i = 0; i < names.length; i++) {
   new Think(names[i]);
+}
+function resluts ()
+{
+  for (var d=0; d<names.length; d++){
+    // product=Think.add[d].name.split(".",1);
+    // console.log("prdduct",product);
+    tClick.push(Think.all[d].clicks);
+    console.log("aaaa",tClick);
+    total.push(Think.all[d].views);
+    console.log("ss",total);
+    
+  }
 }
 
 var leftimg, rightimg, centerimg
-function render () {
+function render() {
+  // check if there is an old iamges if so compere it with the new one and remove the old 3
+ 
+  // push 3 images
 
-   leftimg = Think.all[randomNumber(0,Think.all.length-1)];
-   rightimg = Think.all[randomNumber(0,Think.all.length-1)];
-  centerimg = Think.all[randomNumber(0,Think.all.length-1)];
-while((leftimg ===centerimg) || (leftimg === rightimg) ||(centerimg === rightimg))
-{
-render();
+ // display the images
+  leftimg = Think.all[randomNumber(0, Think.all.length - 1)];
+  rightimg = Think.all[randomNumber(0, Think.all.length - 1)];
+  centerimg = Think.all[randomNumber(0, Think.all.length - 1)];
+  while ((leftimg === centerimg) || (leftimg === rightimg) || (centerimg === rightimg)) {
+   
+    render();
+  }
+  leftImage.setAttribute('src', leftimg.imagePath);
+  leftImage.setAttribute('alt', (leftimg.name.split(".",1)));
+  leftImage.setAttribute('title', (leftimg.name.split(".",1)));
+  rightImage.setAttribute('src', rightimg.imagePath);
+  rightImage.setAttribute('alt', (rightimg.imagePath.split(".",1)));
+  rightImage.setAttribute('title', (rightimg.imagePath.split(".",1)));
+  centerImage.setAttribute('src', centerimg.imagePath);
+  centerImage.setAttribute('alt', (centerimg.imagePath.split(".",1)));
+  centerImage.setAttribute('title', (centerimg.imagePath.split(".",1)));
 }
-  leftImage.setAttribute('src',leftimg.imagePath);
-  leftImage.setAttribute('alt',leftimg.name);
-  leftImage.setAttribute('title',leftimg.name);
-  rightImage.setAttribute('src',rightimg.imagePath);
-  rightImage.setAttribute('alt',rightimg.name);
-  rightImage.setAttribute('title',rightimg.name);
-  centerImage.setAttribute('src',centerimg.imagePath);
-  centerImage.setAttribute('alt',centerimg.name);
-  centerImage.setAttribute('title',centerimg.name);
-}
 render();
-
-imageSection.addEventListener('click',handleClickOnimg);
-var totalClicks =0;
+imageSection.addEventListener('click', handleClickOnimg);
+var totalClicks = 0;
 function handleClickOnimg(event) {
-if(totalClicks <25 ) {
-  // while(leftimg === centerimg || leftimg === rightimg || rightimg === centerimg || rightimg === leftimg)  
+  totalView ++;
 
-  // {
+  saveValues();
+  getValues();
+  if (totalClicks < 25) {
+ 
 
-  //   render();
-  // }
-  
-  
-    if(event.target.id !== 'imagesSection') {
-      if(event.target.id === 'leftImage') {
+    if (event.target.id !== 'imagesSection') {
+      if (event.target.id === 'leftImage') {
         leftimg.clicks++;
-      } else if(event.target.id === 'rightImage') {
+        
+
+
+      } else if (event.target.id === 'rightImage') {
         rightimg.clicks++;
-      }else if (event.target.id === 'centerImage'){
-          centerimg.clicks++;
-          console.log(centerimg.clicks);
+      } else if (event.target.id === 'centerImage') {
+        centerimg.clicks++;
+        console.log(centerimg.clicks);
       }
       totalClicks++;
       leftimg.views++;
-      rightimg.views++;}
-      render();
-      console.log(totalClicks);
-    }else
-  {
-    console.log('more than 25 clicks');
-    imageSection.removeEventListener('click',handleClickOnimg);
-    render2();
-  }}
+      rightimg.views++;
 
+//       tClick.push(Think.all.views);
+// console.log("dddd",tClick );
+    }
+
+    render();
+    console.log(totalClicks);
+  
+
+  } else {
+    resluts();
+    console.log('more than 25 clicks');
+    for ( var g=0; i<names.length; g++){
+      tClick.push(Think.all[g].clicks);
+      console.log("val",tClick);
+    }
+    imageSection.removeEventListener('click', handleClickOnimg);
+    render2();
+  }
+}
 
 function render2() {
-  var ulE1 = document.getElementById('summary');
-  for (var i =0; i<Think.all.length ; i++) {
-    var liE1 = document.createElement('li');
-    liE1.textContent = `${Think.all[i].name} has ${Think.all[i].clicks} clicks and ${Think.all[i].views} views`;
-    ulE1.appendChild(liE1);
+  // var ulE1 = document.getElementById('summary');
+  // for (var i = 0; i < Think.all.length; i++) {
+  //   var liE1 = document.createElement('li');
+  //   liE1.textContent = `${Think.all[i].name} has ${Think.all[i].clicks} clicks and ${Think.all[i].views} views`;
+  //   ulE1.appendChild(liE1);
+  // }
+  
+ 
+  var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart (ctx,{
+  type: 'bar',
+  data:{
+    labels: product,
+    datasets :[{
+      label: "# of Clicks",
+      data :tClick,
+      backgroundColor: 
+      'rgba(255, 99, 132, 0.2)',
+      
+
+  borderColor: [
+      'rgba(255, 99, 132, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)'
+  ],
+  borderWidth: 1
+      
+
+    },{ label: " # of Views",
+    data :total,
+    backgroundColor:[
+    'rgba(255, 99, 132, 0.2)'],
+  },],
+  options: {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    }
+}
+
   }
+});
 }
 
 
